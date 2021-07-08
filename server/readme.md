7b408481cfae98da9523eb58aa0846119a2a7c30
Adem Cemil YALCIN <acyalcin@netrd.com.tr>
2021-07-08 14:40:20 +0300

    init

---
+++ server/readme.md
@@ -0,0 +1,2 @@
+# RUN with docker run command
+`docker run -p 3000:3020 --env-file ./.env acemilyalcin/server:v2`
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json([
+      {id:1, productName: 'Computer'},
+      {id:2, productName: 'Keyboard'},
+      {id:3,productName: 'Mouse'},
+      {id:4,productName: 'Monitor'}
+    ])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
